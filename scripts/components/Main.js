60a14fb3aedbfe0a60828fc337ae65e676bae96d
julia <s98435@yandex.ru>
2022-05-07 13:41:01 +0300

    fix: clean master branch

--- scripts/components/Main.js
+++
@@ -1,8 +0,0 @@
-import createElement from './createElement.js';
-
-export default class Main {
-  constructor() {
-    this.template = '<main class="main"></main>';
-    this.elem = createElement(this.template);
-  }
-}
